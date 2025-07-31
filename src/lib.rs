3141ea27a4c73c4d1722a6f099c4c55ecc214677
Ach Belkhiria <a.belkhiria@enviosystems.com>
2025-07-31 10:18:01 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,193 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{
+    parse_macro_input, punctuated::Punctuated, Expr, ExprLit, ExprPath, ItemFn, Lit, Meta, MetaNameValue, Token,
+};
+
+#[proc_macro_attribute]
+pub fn on_ok(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(attr as Expr);
+    let input_fn = parse_macro_input!(item as ItemFn);
+
+    let on_ok_fn: Option<ExprPath> = if let Expr::Lit(ExprLit {
+        lit: Lit::Str(lit_str), ..
+    }) = args
+    {
+        Some(lit_str.parse().unwrap())
+    } else {
+        None
+    };
+
+    let on_ok = on_ok_fn.expect("Missing callback");
+
+    let vis = &input_fn.vis;
+    let sig = &input_fn.sig;
+    let block = &input_fn.block;
+
+    let gen = quote! {
+        #vis #sig {
+            let result = (|| #block)();
+            match &result {
+                Ok(_) => #on_ok(),
+                Err(_) => (),
+            }
+            result
+        }
+    };
+
+    gen.into()
+}
+
+#[proc_macro_attribute]
+pub fn on_result(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(attr with Punctuated::<Meta, Token![,]>::parse_terminated);
+    let input_fn = parse_macro_input!(item as ItemFn);
+
+    // Default callback names
+    let mut on_ok_fn: Option<ExprPath> = None;
+    let mut on_err_fn: Option<ExprPath> = None;
+
+    for arg in args {
+        if let Meta::NameValue(MetaNameValue {
+            path,
+            value: Expr::Lit(ExprLit {
+                lit: Lit::Str(lit_str), ..
+            }),
+            ..
+        }) = arg
+        {
+            let ident = path.get_ident().map(|i| i.to_string());
+            match ident.as_deref() {
+                Some("on_ok") => on_ok_fn = Some(lit_str.parse().unwrap()),
+                Some("on_err") => on_err_fn = Some(lit_str.parse().unwrap()),
+                _ => {}
+            }
+        }
+    }
+
+    let on_ok_call = on_ok_fn.map(|f| quote!(#f()));
+    let on_err_call = on_err_fn.map(|f| quote!(#f()));
+
+    let vis = &input_fn.vis;
+    let sig = &input_fn.sig;
+    let block = &input_fn.block;
+
+    let gen = quote! {
+        #vis #sig {
+            let result = (|| #block)();
+            match &result {
+                Ok(_) => { #on_ok_call }
+                Err(_) => { #on_err_call }
+            }
+            result
+        }
+    };
+
+    gen.into()
+}
+
+#[proc_macro_attribute]
+pub fn retry(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(attr with Punctuated::<Meta, Token![,]>::parse_terminated);
+    let input_fn = parse_macro_input!(item as ItemFn);
+
+    let mut times = 0;
+    let mut delay_ms = 0;
+
+    for arg in args {
+        if let Meta::NameValue(MetaNameValue {
+            path,
+            value: Expr::Lit(ExprLit {
+                lit: Lit::Int(lit_int), ..
+            }),
+            ..
+        }) = arg
+        {
+            let ident = path.get_ident().map(|i| i.to_string());
+            match ident.as_deref() {
+                Some("times") => times = lit_int.base10_parse::<usize>().unwrap(),
+                Some("delay_ms") => delay_ms = lit_int.base10_parse::<u64>().unwrap(),
+                _ => {}
+            }
+        }
+    }
+
+    let vis = &input_fn.vis;
+    let sig = &input_fn.sig;
+    let block = &input_fn.block;
+
+    let delay_call = if delay_ms > 0 {
+        quote! {
+            std::thread::sleep(std::time::Duration::from_millis(#delay_ms));
+        }
+    } else {
+        quote! {}
+    };
+
+    let gen = quote! {
+        #vis #sig {
+            let mut result;
+            for i in 0..#times {
+                result = (|| #block)();
+                if result.is_ok() {
+                    return result;
+                }
+                if i < #times - 1 {
+                    #delay_call
+                }
+            }
+            (|| #block)()
+        }
+    };
+
+    gen.into()
+}
+
+#[proc_macro_attribute]
+pub fn timeout(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(attr with Punctuated::<Meta, Token![,]>::parse_terminated);
+    let input_fn = parse_macro_input!(item as ItemFn);
+
+    let mut duration_ms = 0;
+
+    for arg in args {
+        if let Meta::NameValue(MetaNameValue {
+            path,
+            value: Expr::Lit(ExprLit {
+                lit: Lit::Int(lit_int), ..
+            }),
+            ..
+        }) = arg
+        {
+            let ident = path.get_ident().map(|i| i.to_string());
+            match ident.as_deref() {
+                Some("duration_ms") => duration_ms = lit_int.base10_parse::<u64>().unwrap(),
+                _ => {}
+            }
+        }
+    }
+
+    let vis = &input_fn.vis;
+    let sig = &input_fn.sig;
+    let block = &input_fn.block;
+
+    let gen = quote! {
+        #vis #sig {
+            let (sender, receiver) = std::sync::mpsc::channel();
+            let handle = std::thread::spawn(move || {
+                let result = (|| #block)();
+                sender.send(result).unwrap();
+            });
+
+            match receiver.recv_timeout(std::time::Duration::from_millis(#duration_ms)) {
+                Ok(result) => result,
+                Err(std::sync::mpsc::RecvTimeoutError::Timeout) => {
+                    Err(format!("Function timed out after {}ms", #duration_ms))
+                },
+                Err(e) => Err(format!("Channel error: {}", e)),
+            }
+        }
+    };
+
+    gen.into()
+}
---
+++ tests/integration_test.rs
@@ -0,0 +1,182 @@
+use decorator::{on_ok, on_result, retry, timeout};
+use std::cell::RefCell;
+
+thread_local! {
+    static OK_CALLED: RefCell<bool> = RefCell::new(false);
+    static ERR_CALLED: RefCell<bool> = RefCell::new(false);
+    static RETRY_COUNT: RefCell<u32> = RefCell::new(0);
+}
+
+fn on_ok_callback() {
+    OK_CALLED.with(|c| *c.borrow_mut() = true);
+}
+
+fn on_err_callback() {
+    ERR_CALLED.with(|c| *c.borrow_mut() = true);
+}
+
+fn reset_callbacks() {
+    OK_CALLED.with(|c| *c.borrow_mut() = false);
+    ERR_CALLED.with(|c| *c.borrow_mut() = false);
+    RETRY_COUNT.with(|c| *c.borrow_mut() = 0);
+}
+
+#[test]
+fn test_on_ok_decorator_with_ok_result() {
+    reset_callbacks();
+
+    #[on_ok("on_ok_callback")]
+    fn function_that_returns_ok() -> Result<(), ()> {
+        Ok(())
+    }
+
+    let _ = function_that_returns_ok();
+    assert!(OK_CALLED.with(|c| *c.borrow()));
+}
+
+#[test]
+fn test_on_ok_decorator_with_err_result() {
+    reset_callbacks();
+
+    #[on_ok("on_ok_callback")]
+    fn function_that_returns_err() -> Result<(), ()> {
+        Err(())
+    }
+
+    let _ = function_that_returns_err();
+    assert!(!OK_CALLED.with(|c| *c.borrow()));
+}
+
+#[test]
+fn test_on_result_decorator_with_ok_result() {
+    reset_callbacks();
+
+    #[on_result(on_ok = "on_ok_callback", on_err = "on_err_callback")]
+    fn function_that_returns_ok() -> Result<(), ()> {
+        Ok(())
+    }
+
+    let _ = function_that_returns_ok();
+    assert!(OK_CALLED.with(|c| *c.borrow()));
+    assert!(!ERR_CALLED.with(|c| *c.borrow()));
+}
+
+#[test]
+fn test_on_result_decorator_with_err_result() {
+    reset_callbacks();
+
+    #[on_result(on_ok = "on_ok_callback", on_err = "on_err_callback")]
+    fn function_that_returns_err() -> Result<(), ()> {
+        Err(())
+    }
+
+    let _ = function_that_returns_err();
+    assert!(!OK_CALLED.with(|c| *c.borrow()));
+    assert!(ERR_CALLED.with(|c| *c.borrow()));
+}
+
+#[test]
+fn test_on_result_decorator_with_missing_on_ok() {
+    reset_callbacks();
+
+    #[on_result(on_err = "on_err_callback")]
+    fn function_that_returns_ok() -> Result<(), ()> {
+        Ok(())
+    }
+
+    let _ = function_that_returns_ok();
+    assert!(!OK_CALLED.with(|c| *c.borrow()));
+    assert!(!ERR_CALLED.with(|c| *c.borrow()));
+}
+
+#[test]
+fn test_on_result_decorator_with_missing_on_err() {
+    reset_callbacks();
+
+    #[on_result(on_ok = "on_ok_callback")]
+    fn function_that_returns_err() -> Result<(), ()> {
+        Err(())
+    }
+
+    let _ = function_that_returns_err();
+    assert!(!OK_CALLED.with(|c| *c.borrow()));
+    assert!(!ERR_CALLED.with(|c| *c.borrow()));
+}
+
+#[test]
+fn test_retry_decorator() {
+    reset_callbacks();
+
+    #[retry(times = 3)]
+    fn function_that_fails_twice() -> Result<(), ()> {
+        RETRY_COUNT.with(|c| *c.borrow_mut() += 1);
+        if RETRY_COUNT.with(|c| *c.borrow()) < 3 {
+            Err(())
+        } else {
+            Ok(())
+        }
+    }
+
+    let result = function_that_fails_twice();
+    assert!(result.is_ok());
+    assert_eq!(RETRY_COUNT.with(|c| *c.borrow()), 3);
+}
+
+#[test]
+fn test_retry_decorator_that_always_fails() {
+    reset_callbacks();
+
+    #[retry(times = 3)]
+    fn function_that_always_fails() -> Result<(), ()> {
+        RETRY_COUNT.with(|c| *c.borrow_mut() += 1);
+        Err(())
+    }
+
+    let result = function_that_always_fails();
+    assert!(result.is_err());
+    assert_eq!(RETRY_COUNT.with(|c| *c.borrow()), 4);
+}
+
+#[test]
+fn test_retry_decorator_with_delay() {
+    reset_callbacks();
+
+    #[retry(times = 3, delay_ms = 10)]
+    fn function_that_fails_twice() -> Result<(), ()> {
+        RETRY_COUNT.with(|c| *c.borrow_mut() += 1);
+        if RETRY_COUNT.with(|c| *c.borrow()) < 3 {
+            Err(())
+        } else {
+            Ok(())
+        }
+    }
+
+    let result = function_that_fails_twice();
+    assert!(result.is_ok());
+    assert_eq!(RETRY_COUNT.with(|c| *c.borrow()), 3);
+}
+
+#[test]
+fn test_timeout_decorator_success() {
+    #[timeout(duration_ms = 10)]
+    fn function_that_finishes_quickly() -> Result<(), String> {
+        Ok(())
+    }
+
+    let result = function_that_finishes_quickly();
+    assert!(result.is_ok());
+}
+
+#[test]
+fn test_timeout_decorator_timeout() {
+    #[timeout(duration_ms = 10)]
+    fn function_that_takes_too_long() -> Result<(), String> {
+        std::thread::sleep(std::time::Duration::from_millis(50));
+        Ok(())
+    }
+
+    let result = function_that_takes_too_long();
+    assert!(result.is_err());
+    assert_eq!(result.unwrap_err(), "Function timed out after 10ms");
+}
+
